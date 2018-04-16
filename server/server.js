33fe55a1fd58ac6044d09bd6418648861442e4a7
Ray Chan <ray25222@gmail.com>
2018-04-16 18:45:27 +0800

    change port env

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
 	console.log(`Server started on PORT: ${PORT}`);
 });
